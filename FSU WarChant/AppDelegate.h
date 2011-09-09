e19f107029e9d03cac093d9f6bb13d0897083d7f
Richard Kirk <RichardBKirk@gmail.com>
2011-09-09 14:19:57 -0500

    Initial Commit

---
+++ FSU WarChant/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FSU WarChant
+//
+//  Created by Richard Kirk on 9/9/11.
+//  Copyright (c) 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ FSU WarChant/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FSU WarChant
+//
+//  Created by Richard Kirk on 9/9/11.
+//  Copyright (c) 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
